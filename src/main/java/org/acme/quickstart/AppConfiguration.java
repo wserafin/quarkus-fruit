ece320e7b0e29144e0a72e291d5d91c8adf57132
wserafin <wserafin@gmail.com>
2020-03-13 17:56:01 -0400

    First Commit

---
+++ src/main/java/org/acme/quickstart/AppConfiguration.java
@@ -0,0 +1,13 @@
+package org.acme.quickstart;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean(name = "capitalizeFunction")
+    public StringFunction capitalizer() {
+        return String::toUpperCase;
+    }
+}
---
+++ src/main/java/org/acme/quickstart/FruitRepository.java
@@ -0,0 +1,11 @@
+package org.acme.quickstart;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface FruitRepository extends CrudRepository<Fruit, Long> {
+
+    List<Fruit> findByColor(String color);
+
+}
